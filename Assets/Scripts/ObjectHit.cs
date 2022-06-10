4466ff95a016e8c88dc6b5878649c338756fcb29
medicen420 <50994647+medicen420@users.noreply.github.com>
2022-06-10 12:15:51 -0500

    GetComponent<>
    Hemos aprendido en el curso a usar GetComponent, para poder acceder a las propiedades de la pared en este caso, buscando ajustar el color del material cuando una collision ocurra.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -8,7 +8,10 @@ public class ObjectHit : MonoBehaviour
     //En el momento en que entra en la colisión ejecuta el siguiente código
       private void OnCollisionEnter(Collision other)
       {
-          print("<color=green> Bumped in to the wall </color>");
+        print("<color=blue> Bumped in to the wall </color>");
+        //Dentro de el Componente 
+        GetComponent<MeshRenderer>().material.color = Color.red;
+
       }
 
     
